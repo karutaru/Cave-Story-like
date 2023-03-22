f1ca067586cee7ae8f0c5a80d1e531a92a64751d
karutaru <atmember03@gmail.com>
2023-03-22 23:57:52 +0900

    エネミーの経路探索で床を認識できるように改善

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -81,12 +81,14 @@ public class EnemyAI : MonoBehaviour
         {
             if (direction.y > jumpNodeHeightRequirement)
             {
-                rb.AddForce(Vector2.up * jumpSpeed * jumpModifier);
+                //rb.AddForce(Vector2.up * jumpSpeed * jumpModifier);
+                rb.AddForce(new Vector2 (0, jumpSpeed));
             }
         }
 
         //移動
-        rb.AddForce(force);
+        //rb.AddForce(force);
+        this.gameObject.transform.localPosition = new Vector2 (this.gameObject.transform.localPosition.x + 0.04f * -transform.localScale.x, this.gameObject.transform.localPosition.y);
 
         //Next Waypoint
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
@@ -98,11 +100,11 @@ public class EnemyAI : MonoBehaviour
         //見ている方向の処理
         if (directionLookEnabled)
         {
-            if (rb.velocity.x > 0.05f)
+            if (direction.x > 0.05f)
             {
                 transform.localScale = new Vector3(-1f * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
-            else if (rb.velocity.x < -0.05f)
+            else if (direction.x < -0.05f)
             {
                 transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
