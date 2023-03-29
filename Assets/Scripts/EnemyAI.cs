0f9282160261e51eebede9a48d2f150ef21c7be5
karutaru <atmember03@gmail.com>
2023-03-29 19:08:56 +0900

    EnemyAIスクリプトの整理 その2

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,67 +4,70 @@ using UnityEngine;
 using Pathfinding;
 using DG.Tweening;
 
+/// <summary>
+/// エネミーの行動に関するスクリプト
+/// </summary>
 public class EnemyAI : MonoBehaviour
 {
     [Header("ステータス")]
-    public int hp = 5;
-    public int attack = 2;
-    public float speed = 40f;
-    public float jumpPower = 20f;
+    public int hp = 5;                          // 最大HP
+    public int attack = 2;                      // 攻撃力
+    public float speed = 40f;                   // 足の速さ
+    public float jumpPower = 20f;               // ジャンプ力
 
     [Header("経路探索")]
-    public Transform target;
-    public float activateDistance = 50f;
-    public float pathUpdateSeconds = 0.5f;
+    public Transform target;                    // ターゲットの位置
+    public float activateDistance = 50f;        // 索敵距離
+    public float pathUpdateSeconds = 0.5f;      //パスの更新時間
 
     [Header("パス挙動")]
     public float nextWayPointDistance = 3f;
     public float jumpNodeHeightRequirement = 0.8f;
 
     [Header("行動の許可")]
-    public bool followEnabled = true; //追跡が可能
-    public bool jumpEnabled = true; //ジャンプが可能
-    public bool directionLookEnabled = true; //方向変化が可能
-    public bool walkEnabled = true; //歩行が可能
-    public bool stepSound = false; //歩行サウンドを鳴らす
-    public bool attackEnabled = false; //攻撃が可能
-    public bool flyEnabled = false; //飛行が可能
+    public bool followEnabled = true;           // 追跡が可能
+    public bool jumpEnabled = true;             // ジャンプが可能
+    public bool directionLookEnabled = true;    // 方向変化が可能
+    public bool walkEnabled = true;             // 歩行が可能
+    public bool stepSound = false;              // 歩行サウンドを鳴らす
+    public bool attackEnabled = false;          // 攻撃が可能
+    public bool flyEnabled = false;             // 飛行が可能
 
     [Header("音 関連")]
-    public AudioClip walkSE;
-    public AudioClip idleSE;
-    public AudioClip deathSE;
-    public float walkSoundCounter;
+    public AudioClip walkSE;                    // 歩くSE
+    public AudioClip idleSE;                    // 立ち止まるSE
+    public AudioClip deathSE;                   // 死亡時のSE
+    public float walkSoundCounter;              // 足音の鳴る感覚
 
     [Header("プレハブ 関連")]
-    public GameObject ExplosionFirstPrefab;
-    public GameObject ExplosionPrefab;
+    public GameObject ExplosionFirstPrefab;     // 最初の大きな爆発のプレハブ
+    public GameObject ExplosionPrefab;          // 次点の細かな爆発のプレハブ
 
     [Header("システム")]
-    public LayerMask groundLayer;
+    public LayerMask groundLayer;               // 接地判定用のレイヤー
 
     //--------------------------動的な変数-------------------------------
 
     [Header("※変数確認用")]
-    public int currentHP;
-    public bool canAttack = false;
+    public int currentHP;                       // 現在のHP
+    public bool canAttack = false;              // 攻撃が可能か
 
     //-----------------------------雑多----------------------------------
 
-    private Animator anim;
+    Seeker seeker;
     Rigidbody2D rb;
-    private Path path; //パス
-    Seeker seeker; //ターゲットを見る
+    private Path path;
+    private Animator anim;
 
-    private bool isGrounded = false; //接地判定
-    private int currentWaypoint = 0;
-    private int damage;
+    private float distance;
     private float jumpTimer;
     private float walkTimer;
+    private int damage;
     private int moveTimer;
-    private bool jumping;
+    private int currentWaypoint = 0;
     private bool canStep;
-    private float distance;
+    private bool jumping;
+    private bool isGrounded = false; // 接地判定
     private Vector2 direction;
     private BulletController bulletController;
 
@@ -73,51 +76,66 @@ public class EnemyAI : MonoBehaviour
 
     public void Start()
     {
-        rb =GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        seeker = GetComponent<Seeker>();
+        if(!TryGetComponent(out rb)) return;
+        if(!TryGetComponent(out anim)) return;
+        if(!TryGetComponent(out seeker)) return;
+ 
+        Debug.Log("各変数の取得完了");
 
         InvokeRepeating("UpdatePath", 0f, pathUpdateSeconds);
 
+        //現在のHPを最大値まで回復
         currentHP = hp;
     }
 
     void Update()
     {
-        isjumpTimer(); //ジャンプしているか
+        // ジャンプの判定
+        isjumpTimer();
     }
 
     private void FixedUpdate()
     {
-        if (TargetInDistance() && followEnabled) //ターゲットとの距離が近く、追跡できる場合
+        // ターゲットとの距離が近く、追跡できる場合
+        if (InDistanceTarget() && followEnabled)
         {
-            PathFollow(); //動きの大きな処理
+            // 移動先決定
+            FollowPath();
 
-            MoveStart();//動き出した時
+            // 移動開始
+            StartMove();
         }
 
         else {
-            MoveStop(); //動かない時
+            // 移動停止
+            StopMove();
         }
 
-
-        if (stepSound && canStep) //歩行SEがあり、あるいている時
+        // 歩行SEがあり、あるいている時
+        if (stepSound && canStep)
         {
-            WalkSound();
+            // 歩くSEを鳴らす
+            IsWalkSound();
         }
     }
 
     //--------------------------------------------------------------↓-パス関連-↓----------------------------------------------------------------------
 
+    /// <summary>
+    /// パスの更新
+    /// </summary>
     private void UpdatePath()
     {
-        if (followEnabled && TargetInDistance() && seeker.IsDone())
+        if (followEnabled && InDistanceTarget() && seeker.IsDone())
         {
             seeker.StartPath(rb.position, target.position, OnPathComplete);
         }
     }
 
-    private void PathFollow() //追跡できる場合
+    /// <summary>
+    /// 追跡可能
+    /// </summary>
+    private void FollowPath()
     {
         if (path == null)
         {
@@ -129,26 +147,34 @@ public class EnemyAI : MonoBehaviour
             return;
         }
         
-        PathDirection(); //Direction計算へ
+        // Direction計算へ
+        PathDirection();
 
-        Jump(); //ジャンプへ
+        // ジャンプへ
+        Jump();
 
-        Move(); //移動へ
+        // 移動へ
+        Move();
 
-        Fly(); //飛行へ
+        // 飛行へ
+        Fly();
 
-        Attack(); //攻撃へ
+        // 攻撃へ
+        Attack();
 
-        NextWayPointDistance(); //次のWaypointの計算へ
+        // 次のWaypointの計算へ
+        NextWayPointDistance();
 
-
-        LookDirection(); //見ている方向の処理へ
+        // 見ている方向の処理へ
+        LookDirection();
     }
 
     //-------------------------------------------------------------------↓-移動関連-↓-------------------------------------------------------------------------
 
 
-    //移動
+    /// <summary>
+    /// 移動
+    /// </summary>
     void Move()
     {
         //移動
@@ -156,7 +182,9 @@ public class EnemyAI : MonoBehaviour
         {
             if (anim != null)
             {
+                // 歩くアニメーションを再生
                 anim.Play("Walk");
+
                 canStep = true;
             }
             //rb.AddForce(force);
@@ -164,29 +192,37 @@ public class EnemyAI : MonoBehaviour
 
         } else //0.04f
         {
-            Debug.Log("Idle");
+            // 立ち止まるアニメーションを再生
             anim.Play("Idle");
         }
     }
 
-    //動き出した時
-    void MoveStart()
+    /// <summary>
+    /// 動き出し
+    /// </summary>
+    void StartMove()
     {
         moveTimer = 0;
     }
 
-    //動かない時
-    void MoveStop()
+    /// <summary>
+    /// 移動停止
+    /// </summary>
+    void StopMove()
     {
         if (anim != null && moveTimer == 0)
                 {
-                    anim.Play("Idle"); //止まるアニメーション
+                    // 止まるアニメーション
+                    anim.Play("Idle");
+
                     canStep = false;
 
                 }
             if (idleSE != null && moveTimer == 0)
                 {
-                    AudioSource.PlayClipAtPoint(idleSE, transform.position); //止まるSEを再生
+                    // 止まるSEを再生
+                    AudioSource.PlayClipAtPoint(idleSE, transform.position);
+
                     canStep = false;
                     moveTimer = 0;
                 }
@@ -194,28 +230,32 @@ public class EnemyAI : MonoBehaviour
     }
 
 
-    //ジャンプ
+    /// <summary>
+    /// ジャンプ
+    /// </summary>
     void Jump()
     {
-        //接地判定
+        // 接地判定
         isGrounded = Physics2D.Linecast(transform.position + transform.up * -0.2f, transform.position - transform.up * 0.6f, groundLayer);
         Debug.DrawLine(transform.position + transform.up * -0.2f, transform.position - transform.up * 0.5f, Color.red, 0.5f);
 
-        //ジャンプ判定
+        // ジャンプ判定
         if (jumpEnabled && isGrounded && jumping == false)
         {
             if (direction.y > jumpNodeHeightRequirement)
             {
                 jumping = true;
 
-                //ジャンプ実行
+                // ジャンプ実行
                 rb.AddForce(transform.up * jumpPower * 10f);
             }
         }
     }
 
-    //足音
-    private void WalkSound()
+    /// <summary>
+    /// 足音
+    /// </summary>
+    private void IsWalkSound()
     {
         walkTimer += Time.deltaTime;
         if (walkTimer >= walkSoundCounter)
@@ -225,7 +265,9 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    //ジャンプしているか
+    /// <summary>
+    /// ジャンプしているか
+    /// </summary>
     void isjumpTimer()
     {
         if (jumping == true)
@@ -239,16 +281,21 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    //飛行
+    /// <summary>
+    /// 飛行
+    /// </summary>
     void Fly()
     {
-        if (flyEnabled == true) //飛行が許可されているなら
+        // 飛行が許可されているなら
+        if (flyEnabled == true)
         {
-            //障害物以外を移動できるパス処理
+            // 障害物以外を移動できるパス処理
         }
     }
 
-    //見ている方向の処理
+    /// <summary>
+    /// 見ている方向の処理
+    /// </summary>
     void LookDirection()
     {
         if (directionLookEnabled)
@@ -266,35 +313,47 @@ public class EnemyAI : MonoBehaviour
 
     //------------------------------------------↓-固有の行動-↓----------------------------------------------
 
+    /// <summary>
+    /// 攻撃関連
+    /// </summary>
     void Attack()
     {
-        if (canAttack && attackEnabled) //攻撃可能かつ攻撃が許可されている場合
+        // 攻撃可能かつ攻撃が許可されている場合
+        if (canAttack && attackEnabled)
         {
-            //エネミーの攻撃処理
+            // エネミーの攻撃処理
         }
     }
     
     //--------------------------------------------ステータス関連-----------------------------------------------
 
-    //プレイヤーの弾がぶつかった時
+    // プレイヤーの弾がぶつかった時
     void OnTriggerEnter2D(Collider2D col)
     {
         if (bulletController = col.GetComponent<BulletController>())
         {
-            damage = bulletController.weaponDamage; //プレイヤーの弾からダメージを持ってくる
+            // プレイヤーの弾からダメージを持ってくる
+            damage = bulletController.weaponDamage;
 
-            currentHP -= damage; //現在のHPからダメージ分を引く
+            // 現在のHPからダメージ分を引く
+            currentHP -= damage;
 
-            if (currentHP <= 0) //現在のHPが0以下になったら
+            // 現在のHPが0以下になったら
+            if (currentHP <= 0)
             {
-                AudioSource.PlayClipAtPoint(deathSE, transform.position); //死亡時のSE再生
+                // 死亡時のSE再生
+                AudioSource.PlayClipAtPoint(deathSE, transform.position);
 
-                GameObject ExplosionFirstEffect = Instantiate(ExplosionFirstPrefab, transform.position, Quaternion.identity); //最初の大きな爆発エフェクトを生成
-                GameObject ExplosionEffect = Instantiate(ExplosionPrefab, transform.position, Quaternion.identity); //こまかな爆発エフェクトを生成
+                // 最初の大きな爆発エフェクトを生成
+                GameObject ExplosionFirstEffect = Instantiate(ExplosionFirstPrefab, transform.position, Quaternion.identity);
+                // こまかな爆発エフェクトを生成
+                GameObject ExplosionEffect = Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
 
+                // 生成したエフェクトを破壊
                 Destroy(ExplosionFirstEffect, 0.4f);
                 Destroy(ExplosionEffect, 1f);
 
+                // このオブジェクトを破壊
                 Destroy(this.gameObject);
             }
         }
@@ -302,13 +361,19 @@ public class EnemyAI : MonoBehaviour
 
     //-------------------------------------------↓-パス関連-↓-----------------------------------------------
 
-    //追跡対象との距離が近ければtrueを返す
-    private bool TargetInDistance()
+    /// <summary>
+    /// 追跡対象との距離が近ければtrueを返す
+    /// </summary>
+    /// <returns></returns>
+    private bool InDistanceTarget()
     {
         return Vector2.Distance(transform.position, target.transform.position) < activateDistance;
     }
 
-    //パス完了
+    /// <summary>
+    /// パス完了
+    /// </summary>
+    /// <param name="p"></param>
     private void OnPathComplete(Path p) {
         {
             if (!p.error)
@@ -319,14 +384,18 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    //パスの高さ？
+    /// <summary>
+    /// パスの高さ？
+    /// </summary>
     void PathDirection()
     {
         direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
         Vector2 force = direction * speed * Time.deltaTime;
     }
 
-    //次のWayPointの距離？
+    /// <summary>
+    /// 次のWayPointの距離？
+    /// </summary>
     void NextWayPointDistance()
     {
         //Next Waypoint
@@ -339,7 +408,7 @@ public class EnemyAI : MonoBehaviour
 
     //---------------------------------------↓-その他-↓--------------------------------------------------
 
-    //DOTweenを止める
+    // DOTweenを止める
     void OnCollisionEnter2D(Collision2D col)
     {
         this.transform.DOKill();
