b39ccaf6d28578e06a5a92e60e7eea3fdb28e064
karutaru <atmember03@gmail.com>
2023-04-08 05:18:18 +0900

    アイテムデータの作成とそれを利用したガチャシステム

--- Assets/Scripts/Item/ItemController.cs
+++ Assets/Scripts/Item/ItemController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemController : MonoBehaviour
 {
@@ -10,9 +11,83 @@ public class ItemController : MonoBehaviour
 
     public int playerGetItemID;
 
+    // アイテムの所持数を管理する辞書
+    public Dictionary<int, int> itemCounts = new Dictionary<int, int>();
 
-    public void AddItem(int amount)
+    [SerializeField] private List<Text> itemCountTexts;
+
+    //-----------------------------------------ここまで--------------------------------------------------
+
+    private void Update()
     {
+        // E キーが押されたときの処理
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            DisplayInventory();
 
+
+        }
+    }
+
+    public void AddItem(int itemId, int amount)
+    {
+        if (!itemCounts.ContainsKey(itemId))
+        {
+            itemCounts[itemId] = 0;
+        }
+
+        itemCounts[itemId] += amount;
+
+        UpdateItemCountText(itemId);
+    }
+
+    public int GetItemCount(int itemId)
+    {
+        if (itemCounts.ContainsKey(itemId))
+        {
+            Debug.Log(itemId);
+            return itemCounts[itemId];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    private void DisplayInventory()
+    {
+        Debug.Log("アイテム一覧：");
+        foreach (var item in itemCounts)
+        {
+            Debug.Log("アイテムID: " + item.Key + " 、所持数: " + item.Value);
+        }
+    }
+
+    private void UpdateItemCountText(int itemId)
+    {
+        if (itemDataSO != null)
+        {
+            ItemData itemData = itemDataSO.itemDataList.Find(item => item.itemID == itemId);
+            if (itemData != null)
+            {
+                int index = itemDataSO.itemDataList.IndexOf(itemData);
+                if (index >= 0 && index < itemCountTexts.Count && itemCountTexts[index] != null)
+                {
+                    int totalItemCount = TotalItemCount();
+                    float percentage = (float)itemCounts[itemId] / totalItemCount * 100;
+                    itemCountTexts[index].text = $"アイテムID: {itemId} 、所持数: {itemCounts[itemId]} ({percentage:F1}%)";
+                }
+            }
+        }
+    }
+
+    private int TotalItemCount()
+    {
+        int total = 0;
+        foreach (var item in itemCounts)
+        {
+            total += item.Value;
+        }
+        return total;
     }
 }
--- Assets/Scripts/Item/ItemScript.cs
+++ Assets/Scripts/Item/ItemScript.cs
@@ -11,12 +11,20 @@ public class ItemScript : MonoBehaviour
 
     private Renderer itemRenderer;
     public AudioClip itemGetSE;
+    private Rigidbody2D rb;
 
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         itemRenderer = gameObject.GetComponent<Renderer>();
         StartCoroutine("Flashing");
+
+        int randomValue = Random.Range(-80, 81);
+        int randomUpValue = Random.Range(1, 181);
+
+        rb.AddForce(new Vector3(randomValue, 0, 0));
+        rb.AddForce(new Vector3(0, randomUpValue, 0));
     }
 
     IEnumerator Flashing()
@@ -37,7 +45,8 @@ public class ItemScript : MonoBehaviour
         if (getItem && col.TryGetComponent(out ItemController itemController))
         {
             //AudioSource.PlayClipAtPoint(itemGetSE, transform.position);
-            itemController.AddItem(itemID);
+            // 1‚Â‚ÌƒAƒCƒeƒ€‚ð’Ç‰Á
+            itemController.AddItem(itemID, 1);
 
             Destroy(gameObject);
         }
