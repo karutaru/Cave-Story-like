514c571b9b9c6e17ab2782a914d2f330884f9a2d
karutaru <atmember03@gmail.com>
2023-09-27 13:33:59 +0900

    コミット1

--- Assets/Scripts/Enemy/EnemyDataSO.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "EnemyDataSO", menuName = "Create EnemyDataSO")]
-public class EnemyDataSO : ScriptableObject
-{
-    public List<EnemyData> enemyDataList = new List<EnemyData>();
-}
