d8e66f81347ba27f88752c485e3b4616b8651071
karutaru <atmember03@gmail.com>
2023-10-04 21:19:13 +0900

    プレイヤーの顔に関するスクリプトを少しだけリファクタリング

--- Assets/5_Scripts/Player/PlayerHead.cs
+++ Assets/5_Scripts/Player/PlayerHead.cs
@@ -32,26 +32,27 @@ public class PlayerHead : MonoBehaviour
         }
     }
 
-    void CheckMouseAngle()
+    void CheckMouseAngle() // 角度ごとにプレイヤーの顔を更新
     {
         Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = mousePosition - (Vector2)transform.position;
+        Vector2 direction = mousePosition - (Vector2)transform.position;　// マウスの方向を計算
         float angle = Vector2.SignedAngle(transform.up, direction);
         float angle_down = Vector2.SignedAngle(-transform.up, direction);
 
+
+
         if (angle_down > 110 && angle_down <= 180 && look_L == true)
         {
-            // マウスの位置がプレイヤーの上と右側の90度以内にある場合の処理
+            // マウスの位置がプレイヤーの上と右側の70度以内にある場合の処理
             spriteRenderer.sprite = upRightSprite;
             return;
         }
         if (angle > 0 && angle <= 70 && look_L == false)
         {
-            // マウスの位置がプレイヤーの上と右側の90度以内にある場合の処理
+            // マウスの位置がプレイヤーの上と左側の70度以内にある場合の処理
             spriteRenderer.sprite = upRightSprite;
             return;
         }
-
         if (Mathf.Abs(angle) <= 50)
         {
             // マウスの位置がプレイヤーの上90度以内にある場合の処理
@@ -59,41 +60,37 @@ public class PlayerHead : MonoBehaviour
             {
                 spriteRenderer.sprite = upSprite;
             }
+            return;
         }
-        else
+        if (Mathf.Abs(angle_down) <= 50)
         {
+            // マウスの位置がプレイヤーの下90度以内にある場合の処理
+            if (spriteRenderer && downSprite)
+            {
+                spriteRenderer.sprite = downSprite;
+            }
+            return;
+        }
+        if (angle_down > 40 && angle_down <= 130 && look_L == true)
+        {
+            // マウスの位置がプレイヤーの右側の90度以内にある場合の処理
+            spriteRenderer.sprite = rightSprite;
 
-            if (Mathf.Abs(angle_down) <= 50)
-            {
-                // マウスの位置がプレイヤーの下90度以内にある場合の処理
-                if (spriteRenderer && downSprite)
-                {
-                    spriteRenderer.sprite = downSprite;
-                }
-            }
-            else
-            {
-                if (angle_down > 40 && angle_down <= 130 && look_L == true)
-                {
-                    // マウスの位置がプレイヤーの右側の90度以内にある場合の処理
-                    spriteRenderer.sprite = rightSprite;
-                }
-                else
-                {
-                    if (angle > 40 && angle <= 130 && look_L == false)
-                    {
-                        // マウスの位置がプレイヤーの右側の90度以内にある場合の処理
-                        spriteRenderer.sprite = rightSprite;
-                    }
-                    else
-                    {
-                        if (spriteRenderer && normalSprite)
-                        {
-                            spriteRenderer.sprite = normalSprite;
-                        }
-                    }
-                }
-            }
+            return;
+        }
+        if (angle > 40 && angle <= 130 && look_L == false)
+        {
+            // マウスの位置がプレイヤーの右側の90度以内にある場合で、かつプレイヤーが反転している時の処理
+            spriteRenderer.sprite = rightSprite;
+
+            return;
+        }
+        if (spriteRenderer && normalSprite)
+        {
+            // デフォルトの顔
+            spriteRenderer.sprite = normalSprite;
+
+            return;
         }
     }
 
