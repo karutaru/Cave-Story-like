9390c5b2cf0226618e0fb274ae26ba9265cf2e1b
karutaru <atmember03@gmail.com>
2023-10-04 13:29:08 +0900

    コミット

--- Assets/5_Scripts/Gun/GunRotation.cs
+++ Assets/5_Scripts/Gun/GunRotation.cs
@@ -6,6 +6,19 @@ public class GunRotation : MonoBehaviour
 {
     public Camera mainCamera; // シーン内のカメラ
 
+    public Vector3 EulerAngles { 
+        get { return transform.eulerAngles; }
+        private set { transform.eulerAngles = value; }
+    }
+
+    public Vector3 EulerAngles2
+    {
+        get => transform.eulerAngles;
+        private set => transform.eulerAngles = value;
+    }
+
+    public Quaternion EulerAngles3 => transform.rotation;
+
     void Update()
     {
         if (Time.timeScale == 1)
@@ -20,5 +33,8 @@ public class GunRotation : MonoBehaviour
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.eulerAngles = new Vector3(0, 0, angle);
         }
+
+        //Vector3 angles = EulerAngles; // ﾌﾟﾛﾊﾟﾃｨの使い方
+        //EulerAngles = Vector3.zero;
     }
 }
---
+++ Assets/5_Scripts/Gun/GunRotationCancel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotationCancel : MonoBehaviour
+{
+    
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+    }
+}
