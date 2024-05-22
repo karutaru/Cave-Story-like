abf5b154182179d89766582733253a58286d222c
karutaru <atmember03@gmail.com>
2024-05-22 13:56:16 +0900

    コミット2

--- Assets/5_Scripts/Data/WeaponLevelDataSO.cs
+++ Assets/5_Scripts/Data/WeaponLevelDataSO.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 [CreateAssetMenu(fileName = "WeaponLevelDataSO", menuName = "Create ScriptableObjects/WeaponLevelDataSO")]
 public class WeaponLevelDataSO : ScriptableObject
 {
+    // Odinの属性を使用してリストを編集しやすくする
+    [ListDrawerSettings(Expanded = true, DraggableItems = true, ShowIndexLabels = true, ListElementLabelName = "gunName")]
     public List<WeaponLevelData> weaponLevelDataList = new();
 }
